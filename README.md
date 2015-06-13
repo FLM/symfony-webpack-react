51d62d48f1e0aa742c75b38c67aa869f2fc9901b
jens.paulin <jens.paulin@familjelivmedia.se>
2015-06-13 17:06:25 +0200

    added fallback route and moved example app to /

--- README.md
+++ README.md
@@ -3,4 +3,4 @@ symfony-webpack-react
 
 1. `./install.sh`
 2. `npm start`
-3. Browse `/app_dev.php/app/example`
+3. Browse `/app_dev.php/`
--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -8,7 +8,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 class DefaultController extends Controller
 {
     /**
-     * @Route("/app/example", name="homepage")
+     * @Route("/", name="homepage")
      */
     public function indexAction()
     {
