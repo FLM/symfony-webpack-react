171ae61233cad5ea839cebd9fe0a34185ad26ca5
jens.paulin <jens.paulin@familjelivmedia.se>
2016-05-09 10:50:24 +0200

    create history and store in app.js, with initialState option

--- app/Resources/js/app.js
+++ app/Resources/js/app.js
@@ -1,18 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from 'pages/App';
 import { Provider } from 'react-redux';
 import { Router, Route, IndexRoute } from 'react-router';
-import store from 'store/store';
-import myHistory from 'router/myHistory';
 import { syncHistoryWithStore } from 'react-router-redux';
+import { useRouterHistory } from 'react-router';
+import { createHistory } from 'history';
+import createStore from 'store/createStore';
+import App from 'pages/App';
 import Index from 'pages/Index';
 import Page from 'pages/Page';
 import RouteNotFound from 'pages/RouteNotFound';
 
+const basePath = (__GLOBALS__.dev ? '/app_dev.php' : '');
+const history = useRouterHistory(createHistory)({
+  basename: basePath,
+});
+
+const initialState = {};
+
+const store = createStore(initialState, history);
+const appHistory = syncHistoryWithStore(history, store);
+
 ReactDOM.render(
   <Provider store={store}>
-    <Router history={syncHistoryWithStore(myHistory, store)}>
+    <Router history={appHistory}>
       <Route path="/" component={App}>
         <IndexRoute component={Index} />
         <Route path="page" component={Page} />
---
+++ app/Resources/js/store/createStore.js
@@ -0,0 +1,24 @@
+import { createStore, combineReducers, compose, applyMiddleware } from 'redux';
+import { routerReducer, routerMiddleware } from 'react-router-redux';
+import reducers from 'store/reducers';
+
+export default (initialState = {}, history) => {
+  const middlewares = [routerMiddleware(history)];
+  if (__GLOBALS__.dev) {
+    const createLogger = require('redux-logger');
+    const logger = createLogger();
+    middlewares.push(logger);
+  }
+
+  const storeEnhancers = [];
+  storeEnhancers.push(applyMiddleware(...middlewares));
+
+  const finalCreateStore = compose(...storeEnhancers)(createStore);
+
+  const reducer = combineReducers({
+    ...reducers,
+    routing: routerReducer,
+  });
+
+  return finalCreateStore(reducer, initialState);
+};
